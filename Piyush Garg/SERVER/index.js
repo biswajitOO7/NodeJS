bbd798d87c7c10eb80591c3f4eee821c52af2636
bmajumdar <bmajumdar@xandr.com>
2024-04-28 11:08:27 +0530

    HTTP Methods

--- Piyush Garg/SERVER/index.js
+++ Piyush Garg/SERVER/index.js
@@ -5,18 +5,18 @@ const url = require("url"); // https://www.npmjs.com/package/url -- Documentatio
 
 const myServer = http.createServer((req,res)=>{
     //console.log('New Request received');
-    console.log(req.headers)
+    //console.log(req.headers)
     if(req.url === "/favicon.ico") return res.end();
     // we will use non blocking othrewish all threads will be busy on file reading 
-    const log = `${Date.now()}:${req.url} new req received\n`
+    const log = `${Date.now()}:${req.method}${req.url} new req received\n`;
     const myUrl = url.parse(req.url,true); // true mean We want query parameter string 
     console.log(myUrl);
     fs.appendFile('log.txt',log,(err,data)=>{
         //switch(req.url){
         switch(myUrl.pathname){
-            case '/': res.end("Home page");
-            break
-            case '/about':
+            case "/": res.end("Home page");
+            break;
+            case "/about":
                 const username = myUrl.query.myname; 
                 //res.end("I am Biswajit");
                 res.end(`I am ${username}` );
@@ -24,6 +24,13 @@ const myServer = http.createServer((req,res)=>{
             case "/search" :   // http://localhost:8001/search?search_query=tit+tak+toe
                 const search = myUrl.query.search_query;
                 res.end("Here are your result for "+ search);
+            case "/signup" :
+                if(req.method === "GET") res.end("This is a sign up form");
+                else if(req.method === 'POST') {
+                    // DB Query 
+                    res.end('Success');
+            }
+
             default : res.end("404 Not Found");
         }
         //res.end("Hello From Server");
